03146089c96d2d7bcf22b9bd6fbf7c03577d9901
kota <kjk916@gmail.com>
2026-05-12 02:36:07 +0900

    기능개발

--- src/StackQueue.java
+++ src/StackQueue.java
@@ -1,6 +1,4 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Stack;
+import java.util.*;
 
 public class StackQueue {
     public static void main(String[] args) {
@@ -14,8 +12,10 @@ public class StackQueue {
         // 기능개발
         int[] progresses = {93, 30, 55};
         int[] speeds = {1, 30, 5};
+//        int[] progresses = {95, 90, 99, 99, 80, 99};
+//        int[] speeds = {1, 1, 1, 1, 1, 1};
         int[] ret = sq.solution2(progresses, speeds);
-        System.out.println(Arrays.toString(speeds));
+        System.out.println(Arrays.toString(ret));
 
     }
 
@@ -66,11 +66,60 @@ public class StackQueue {
 
     // 기능개발
     public int[] solution2(int[] progresses, int[] speeds) {
+        /*// 내 풀이
         // 각 작업별 소요일 계산
+        int[] remain = new int[progresses.length];
+        for (int i = 0; i < progresses.length; i++) {
+            remain[i] = (int) Math.ceil((double) (100 - progresses[i]) / speeds[i]);
+        }
 
+        // 배포작업끼리 묶기
+        ArrayList<Integer> list = new ArrayList<>();
+        int count = 1;
+        int preNum = remain[0];
+        for (int i = 1; i < remain.length; i++) {
+            if (preNum >= remain[i]) {
+                count++;
+            } else {
+                list.add(count);
+                preNum = remain[i];
+                count = 1;
+            }
+        }
 
+        // 마지막 남은 작업을 추가해주어야 한다.
+        list.add(count);
 
-        int[] answer = {};
+        int [] answer = new int[list.size()];
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = list.get(i);
+        }*/
+
+        // 추천풀이
+        Queue<Integer> q = new LinkedList<>();
+        List<Integer> answerList = new ArrayList<>();
+
+        for (int i = 0; i < speeds.length; i++) {
+            double remain = (100 - progresses[i]) / (double) speeds[i];
+            int date = (int) Math.ceil(remain);
+
+            if (!q.isEmpty() && q.peek() < date) {
+                answerList.add(q.size());
+                q.clear();
+            }
+
+            q.offer(date);
+        }
+
+        // 마지막 남은 작업을 추가해주어야 한다.
+        answerList.add(q.size());
+
+        int[] answer = new int[answerList.size()];
+
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = answerList.get(i);
+        }
+
         return answer;
     }
 
